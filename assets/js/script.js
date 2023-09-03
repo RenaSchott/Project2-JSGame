bafbf0c53c4dcf4d60d183549ca6e17fbd4a5f3a
R S <rena_schott@gmx.de>
2023-09-03 11:43:32 +0000

    Added js code for flipping cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@
 /**
  * 
  */
+
+//flipping cards - inspired by https://stackoverflow.com/questions/37604456/how-to-make-a-div-flip-on-button-press
+document.getElementById('flip').addEventListener('click', function() {
+    card.classList.toggle('flipped');
+}, false);
