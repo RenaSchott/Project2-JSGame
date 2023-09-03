238b4c4fa8968e7e337d4bda49ed763e77af85b1
R S <rena_schott@gmx.de>
2023-09-03 15:03:53 +0000

    Added html for comparison image

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,33 @@
  * 
  */
 
+
+//random cards - inspired by https://jsbin.com/gazusipeta/1/edit?html,css,js,output
+function shuffleCards() {
+    const grid = document.getElementById("grid");
+    let cards = [...document.getElementsByClassName("card")];
+    cards = cards.sort(() => Math.random() - 0.5);
+  
+    grid.innerHTML = ""
+    cards.forEach(card => grid.appendChild(card));
+  }
+  
 //flipping cards - inspired by https://jsbin.com/deqejekomu/edit?html,css,js,output
-document.getElementById('flip').addEventListener('click', function () {
-    const cards = document.querySelectorAll(".card-inner");
-    cards.forEach(card => card.classList.toggle('flipped'))
-}, false);
+function flipCardsListener() {
+    document.getElementById('flip').addEventListener('click', function () {
+        const cards = document.querySelectorAll(".card-inner");
+        cards.forEach(card => card.classList.toggle('flipped'))
+    }, false);
+  }
+  
+  
+function run() {
+    shuffleCards();
+    flipCardsListener()
+  }
+  run();
+
+
+  //displaying random image - inspired by https://openjavascript.info/2022/12/11/random-image-with-javascript/
+  
+  
