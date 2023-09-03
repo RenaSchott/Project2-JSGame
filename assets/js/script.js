a2e3c0516e69c07936762c32b1104af558f39968
R S <rena_schott@gmx.de>
2023-09-03 15:12:17 +0000

    Added js for showing random image

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,41 @@ function run() {
   run();
 
 
-  //displaying random image - inspired by https://openjavascript.info/2022/12/11/random-image-with-javascript/
-  
+//displaying random image - inspired by https://openjavascript.info/2022/12/11/random-image-with-javascript/
+const imgEl = document.getElementById('comp-image');
+
+const srcArray = ['assets/images/cat.webp', 'assets/images/dog.webp', 'assets/images/elephant.webp'];
+
+imgEl.addEventListener('click', () => {
+    randomImage();
+    // Runs randomImage function when user clicks on image
+});
+
+let index;
+// Current image index
+
+randomImage();
+function randomImage() {
+
+    const randomIndex = Math.floor(Math.random()*srcArray.length);
+    // Generates random number between 0 and array length-1 (because Math.floor rounds)
+
+    if (randomIndex !== index) {
+    // If number NOT same as current image index...
+
+        imgEl.src = srcArray[randomIndex];
+        //...set as image src
+
+        index = randomIndex;
+        //...and as new current index value
+
+    } else {
+    // If number same as current index value (repeat)...
+
+        randomImage();
+        //...start function from beginning again
+
+    }
+
+}
   
