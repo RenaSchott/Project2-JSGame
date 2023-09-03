09ec0890efdf7c48b52f9fdf97e14742e021fa82
R S <rena_schott@gmx.de>
2023-09-03 14:44:53 +0000

    Made images rotate by button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@
  * 
  */
 
-//flipping cards - inspired by https://stackoverflow.com/questions/37604456/how-to-make-a-div-flip-on-button-press
-document.getElementById('flip').addEventListener('click', function() {
-    card.classList.toggle('flipped');
+//flipping cards - inspired by https://jsbin.com/deqejekomu/edit?html,css,js,output
+document.getElementById('flip').addEventListener('click', function () {
+    const cards = document.querySelectorAll(".card-inner");
+    cards.forEach(card => card.classList.toggle('flipped'))
 }, false);
